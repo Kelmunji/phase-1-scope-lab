a3adf6fa7f073fadfb624ecc2fab1457036d39e3
kelvin Githinji <githinjikelvin74@gmail.com>
2025-03-08 19:46:14 +0300

    Adds code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase('bob');
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = 'john Doe';
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer ='Assignment to constant variable.'
+
+}
